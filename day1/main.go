c4459416abd0bee5c3326c180e65b4c3965e6058
Jonas <jonas.motiejunas@gmail.com>
2025-12-09 14:06:34 +0200

    .

--- day1/main.go
+++ day1/main.go
@@ -12,14 +12,15 @@ func parseInput(inputs []string) []int {
 
 	for i := range inputs {
 		input := inputs[i]
-
 		distance, err := strconv.Atoi(input[1:])
 		if err != nil {
 			log.Fatalf("couldn't parse input: %s", err)
 		}
+
 		if input[0:1] == "L" {
 			distance *= -1
 		}
+
 		actions = append(actions, distance)
 	}
 	return actions
@@ -28,12 +29,12 @@ func parseInput(inputs []string) []int {
 func resolveActions(actions []int, position int) int {
 	password := 0
 	position += actions[0] % 100
+
 	if position > 99 {
 		position -= 100
 	} else if position < 0 {
 		position += 100
 	}
-
 	if position == 0 {
 		password++
 	}
@@ -49,6 +50,7 @@ func main() {
 		log.Fatalf("failed to open file: %s", err)
 	}
 	defer file.Close()
+
 	var inputs []string
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
@@ -58,6 +60,7 @@ func main() {
 	if err := scanner.Err(); err != nil {
 		log.Fatalf("error reading file: %s", err)
 	}
+
 	password := resolveActions(parseInput(inputs), 50)
 	log.Printf("Password: %o", password)
 }
